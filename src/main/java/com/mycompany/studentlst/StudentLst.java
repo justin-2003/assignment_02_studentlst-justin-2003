cbc2edb6baf365337aa8dbfc7a354020e1ed7348
Justin  Sam <justinsam02544@gmail.com>
2023-09-16 16:56:25 -0400

    Did all the to do's and did the testing all worked.

--- src/main/java/com/mycompany/studentlst/StudentLst.java
+++ src/main/java/com/mycompany/studentlst/StudentLst.java
@@ -8,27 +8,28 @@ import java.util.Scanner;
 public class StudentLst {
 
     public static void main(String[] args) {
-        Student[] students= new Student[2];
+        Student[] students = new Student[4];
         Scanner inScanner = new Scanner(System.in);
-        
-        for(int i=0; i< students.length;i++){
+
+        for (int i = 0; i < students.length; i++) {
             System.out.print("Enter student's name: ");
             String name = inScanner.nextLine();
             Student student = new Student(name);
-            
-            students[i]= student;
-            
+
+            students[i] = student;
+
         }
         System.out.println(printOutput(students));
     }
-    
-    public static String printOutput(Student[] students){
-        String str="";
-        
+
+    public static String printOutput(Student[] students) {
+        String str = "";
+
         //To Do 03: complete the implementation of this method  
-        
+        System.out.println();
+        for (int i = 0; i < students.length; i++) {
+            str += ("Printing the output " + "The student's name is "+ students[i].getName() +" \n");
+        }
         return str;
     }
-    
-    
 }
